c7b7da86b9564cf7d5e5096663951e4241000afb
Eworitse <williamseworitse@gmail.com>
2022-07-05 17:02:41 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,12 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n = -n;
-	return (n % 10);
+	int last_digit = n % 10;
+	
+	if (last_digit < 0)
+		last_digit *= -1;
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }
