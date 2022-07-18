15dec83bf80db223558a1dbda822c93cc35048ca
Eworitse <williamseworitse@gmail.com>
2022-07-18 14:27:49 -0700

    last task

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_diagmus - prints the sum of the 2 diagonals
+ * print_diagsums - prints the sum of the 2 diagonals
  * of a square matrix of integers
  * @a: the matrix of integers
  * @size: the size of the matrix.
