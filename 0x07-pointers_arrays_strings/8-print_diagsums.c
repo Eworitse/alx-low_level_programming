3cd2798aeb2d86b170f74f2a9e3824892dba57d1
Eworitse <williamseworitse@gmail.com>
2022-07-18 14:25:41 -0700

    task 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,7 +8,7 @@
  * @size: the size of the matrix.
  */
 
-void print_diagmus(int *a, int size)
+void print_diagsums(int *a, int size)
 {
 	int index, sum1 = 0, sum2 = 0;
 
@@ -23,7 +23,7 @@ void print_diagmus(int *a, int size)
 	for (index = 0; index < size; index++)
 	{
 		sum2 += a[index];
-		a -=size;
+		a -= size;
 	}
 
 	printf("%d, %d\n", sum1, sum2);
