c61f6adeddf5105c1d68e04972f27770e1955501
Eworitse <williamseworitse@gmail.com>
2022-07-22 14:07:34 -0700

    task 0 for argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -  function that prints out its name
+ * @argc: argc parameter
+ * @argv: argv parameter
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ * @argc: argc parameter
+ * @argv: an array of the listed command
+ * Return: 0 for success
+ */
